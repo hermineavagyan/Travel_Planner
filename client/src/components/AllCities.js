e4118a24bdf7892447f45f1555e65bd051bf0f04
Hermine <herminemavagyan@gmail.com>
2022-05-13 23:50:57 -0700

    cart incrementer added

--- client/src/components/AllCities.js
+++ client/src/components/AllCities.js
@@ -6,6 +6,8 @@ import Header from "./Header";
 import DeleteButton from "./DeleteButton";
 import MyContext from "./MyContext";
 import Navbar from "./Navbar";
+// import {IconButton} from '@mui/material';
+// import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 
 const AllCities = (props) => {
 
@@ -48,6 +50,25 @@ const AllCities = (props) => {
                 console.log(err);
             })
     }, [])
+
+    const incrementCart = async(e,index)=>{
+        try {
+            const res = await axios.get("http://localhost:8000/api/users", {withCredentials: true})
+                setUser(res.data)
+                context.setCartCount(context.cartCount +=1);
+            await axios.put(`http://localhost:8000/api/users/${user._id}`,
+                {cartCount:context.cartCount,},{withCredentials: true})
+            await axios.post("http://localhost:8000/api/addToCart", 
+                {
+                    "tourName":`${cityList[index].name}`,
+                    "tourImage":`${cityList[index].cityImage}`,
+                    "price":`${cityList[index].price}`,
+                    
+                },{withCredentials: true})
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     const logout = (e) => {
         axios
@@ -144,6 +165,8 @@ const AllCities = (props) => {
                             <p class="card-text">{city.funFact}</p>
                             <p>{city.cityInfo}</p>
                             <p>{city.price}</p>
+                            <button id={index}  aria-label="add to shopping cart" onClick={(e)=>incrementCart(e, index)}>
+                            </button>    
                         </div>
                         
                         <hr style = {{margin: 40, border: 0, backgroundImage: "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(9, 84, 132), rgba(0, 0, 0, 0))", height: 3}}/>
@@ -153,6 +176,8 @@ const AllCities = (props) => {
                             <Link to={`/city/edit/${city._id}`}>
                             <button style = {{paddingRight: 25}} type="button" class="btn btn-secondary btn-sm">Edit</button>
                             </Link></small></p>
+
+
                     </div>
                 </div>
             </div>
